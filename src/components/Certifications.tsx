efb3bd77622d7d054c6ab7c265d29412616fb046
Zoher Hussein <86206923+Zoher45@users.noreply.github.com>
2024-08-17 19:10:34 +1200

    improving responsiveness for phones

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -5,34 +5,36 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
 const Certifications = () => {
-  var items = [
+  const items = [
     {
       name: "Salesforce Certified AI Associate",
       issued: "August 2024",
-      imagePath: "./images/salesforcecertifiedaIassociate.png", //TODO add image
+      imagePath: "./images/salesforcecertifiedaIassociate.png",
     },
     {
       name: "Introduction to Cybersecurity",
       issued: "August 2023",
-      imagePath: "./images/introductiontocybersecurity.png", //TODO add image
+      imagePath: "./images/introductiontocybersecurity.png",
     },
     {
       name: "Salesforce Certified Administrator",
       issued: "July 2023",
-      imagePath: "./images/salesforcecertifiedadministrator.png", //TODO add image
+      imagePath: "./images/salesforcecertifiedadministrator.png",
     },
     {
       name: "Salesforce Certified Associate",
       issued: "June 2023",
-      imagePath: "./images/salesforcecertifiedassociate.png", //TODO add image
+      imagePath: "./images/salesforcecertifiedassociate.png",
     },
   ];
+
   return (
     <Box
       id="certifications"
       sx={{
         textAlign: "center",
         paddingTop: 15,
+        paddingX: { xs: 2, sm: 4 },
         position: "relative",
       }}
     >
@@ -40,35 +42,31 @@ const Certifications = () => {
         Certifications
       </Typography>
 
-      <Grid container direction="row" justifyContent={"center"}>
-        {items.map((item, i) => (
-          <Item key={i} item={item} />
+      <Grid container spacing={2} justifyContent="center">
+        {items.map((item, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Card sx={{ maxWidth: 250, margin: "auto", borderRadius: 2, boxShadow: 3 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.imagePath}
+                alt={item.name}
+                sx={{ objectFit: "contain" }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Issued: {item.issued}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
       </Grid>
     </Box>
   );
 };
-
-function Item(props) {
-  return (
-    <Card sx={{ maxWidth: 250, margin: 1 }}>
-      <CardMedia
-        component="img"
-        height="200"
-        image={props.item.imagePath}
-        alt="Certification Image"
-        sx={{ objectFit: "contain" }}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.item.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Issued: {props.item.issued}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
 
 export default Certifications;
