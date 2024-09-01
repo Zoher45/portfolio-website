52e28e5799f658faf9cd68ff7ffca152b32b9156
zhu45 <zoherazhar@gmail.com>
2024-09-02 00:34:18 +1200

    formatting utils file

--- src/utils/loadBlogPosts.js
+++ src/utils/loadBlogPosts.js
@@ -1,7 +1,7 @@
 export function loadBlogPosts() {
-  const context = require.context('../blog', false, /\.md$/);
+  const context = require.context("../blog", false, /\.md$/);
   return context.keys().map((fileName) => {
-    const slug = fileName.replace('./', '').replace('.md', '');
+    const slug = fileName.replace("./", "").replace(".md", "");
     const content = context(fileName);
     return { slug, content };
   });
